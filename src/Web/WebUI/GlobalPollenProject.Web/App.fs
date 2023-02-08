c90bbd4034f1acd7c60e2cd1da802f67d05216d7
AndrewIOM <AndrewIOM@users.noreply.github.com>
2023-02-08 11:12:01 +0000

    Fix redirect on login page

--- src/Web/WebUI/GlobalPollenProject.Web/App.fs
+++ src/Web/WebUI/GlobalPollenProject.Web/App.fs
@@ -25,7 +25,7 @@ module Actions =
         fun next ctx ->
             let token = ctx.GetTokenAsync("access_token") |> Async.AwaitTask |> Async.RunSynchronously
             ctx.Items.Add("access_token",token)
-            redirectTo true Urls.home next ctx
+            redirectTo false Urls.home next ctx
 
     let logout = signOut "Cookies" >=> redirectTo false "/"
            
